bd5b87ffc0d4a4f185fc2a9fe747db9ce769b103
RichieBooy <riconalvarado@gmail.com>
2018-12-11 18:30:04 -0600

    Se agrego pagina de inicio.html y de empresas.html con el Starter Template de Boostrap

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-let usuario = document.getElementById("usuaio");
-let contraseña = document.getElementById("contraseña");
+// let usuario = document.getElementById("usuario");
+// let contraseña = document.getElementById("contraseña");
+
+// function entrar() {
+//     console.log(contraseña.innerHTML);
+// }
+
+
+
+// let password = false;
+
+
+// function entrar() {
+
+// while (password === false) {
+//     if (usuario === 'usuario@mail.com' && contraseña === 'gatitos13') {
+//         prompt('Contraseña correcta')
+//         password = true;
+//     } else {
+//         alert('Intenta de nuevo')
+//         password = false;
+//     }
+// }
+// }
+
+let contraseña;
 let password = false;
+let correcta;
 
+while (correcta === false) {
+    password = prompt('Ingresa contraseña');
 
-function entrar() {
-
-while (password === false) {
-    if (usuario === 'usuario@mail.com' && contraseña === 'gatitos13') {
-        prompt('Contraseña correcta')
-        password = true;
+    if (contraseña === 'gatitos13') {
+        console.log('Correcta');
+        correcta = true;
     } else {
-        alert('Intenta de nuevo')
-        password = false;
+        console.log('Contraseña incorrecta');
     }
-}
 }
